d5651b1e317e68c684f08e747f2d9590bf786f48
Alan Bantug <ab5383@gmail.com>
2018-02-03 11:47:26 -0800

    feature: Add code to check pattern or whether a combination is a winner
    Added code to check the pattern of the combination generated and check if it is a winner. Then set the color code as stated in the About information

--- displayGenerate.py
+++ displayGenerate.py
@@ -1,12 +1,10 @@
 #! python3
 
-#from tkinter import *
-#from tkinter.ttk import *
-
 import tkinter
 from tkinter import *
 
 from tkinter.ttk import *
+import os
 
 class displayNumbers(object):
 
@@ -18,7 +16,10 @@ class displayNumbers(object):
         self.ext = []
 
         Style().configure("W.TLabel", foreground= "black", background="white", font="Courier 8", anchor="center")
+        Style().configure("L.TLabel", foreground= "white", background="blue", font="Courier 8", anchor="center")
+        Style().configure("Y.TLabel", foreground= "blue", background="yellow", font="Courier 8", anchor="center")
         Style().configure("G.TLabel", foreground= "white", background="green", font="Courier 8", anchor="center")
+        Style().configure("R.TLabel", foreground= "white", background="red", font="Courier 8", anchor="center")
         
         for i in range(self.topLimit):
             idx = "{0:02}".format(i + 1)
@@ -29,12 +30,18 @@ class displayNumbers(object):
 
         Style().configure("W.TLabel", foreground= "black", background="white", font="Courier 8", anchor="center")
 
+        sty = self.checkPattern(topSelect)
+        win = self.checkIfWinner(topSelect)
+
         for i in range(self.topLimit):
             self.num[i]["style"] = "W.TLabel"
 
         for i in range(self.topLimit):
             if i + 1 in topSelect:
-                self.num[i]["style"] = "G.TLabel"
+                if win:
+                    self.num[i]["style"] = "R.TLabel"
+                else:
+                    self.num[i]["style"] = sty
 
 
     def positionTopsDisplays(self, row, col):
@@ -55,8 +62,68 @@ class displayNumbers(object):
 
         row_ctr += 1
         
-        
+    def checkPattern(self, num_set):
 
+        odd_ctr = 0
+
+        for num in num_set:
+            if num % 2 != 0:
+                odd_ctr += 1
+
+        if odd_ctr == 5 or odd_ctr == 0:
+            return "Y.TLabel"
+        elif odd_ctr == 4 or odd_ctr == 1:
+            return "L.TLabel"
+        else:
+            return "G.TLabel"
+
+
+    def checkIfWinner(self, check_set):
+
+        ''' This function will check if the combination is a previous winner
+        '''
+
+        if os.path.isfile("config.txt"):
+            pass
+        else:
+            return False
         
+        configFile = open("config.txt", "r")
+        filename = configFile.readline()
+        configFile.close()
+
+        # If data file does not exist, return False
+        
+        if os.path.isfile(filename):
+            pass
+        else:
+            return False
+        
+        dataFile = open(filename, "r")
+
+        while True:
+        
+            d_line = dataFile.readline()
+
+            if d_line == "":
+                break
+
+            d_list = d_line.split()
+
+            if len(d_list) > 0:
+                if d_list[0].isdigit():
+
+                    winner = []
+                    
+                    for i in range(5, 10):
+                        winner.append(int(d_list[i]))
+
+                    if check_set == winner:
+                        return True
+
+        dataFile.close()
+        return False
+
+
 
         
